89b43e76602018821a609eeb8dfc624a320cd5e2
adityasharan01 <ranjan.aditya2009@gmail.com>
2020-10-10 21:16:39 +0530

    Kaash ho jaye

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
 
             return (
               <Row key={id}>
-                <Col lg={4} sm={12}>
+                <Col lg={4} sm={12} >
                   <Fade
                     left={isDesktop}
                     bottom={isMobile}
